c10c8eb93d3d2e703422a3664d50b431d65fdb19
Cristian <crirodb2@inf.upv.es>
2023-06-11 09:38:46 +0200

    v8 - mejoras en la introducción de datos

--- rompabezas.py
+++ rompabezas.py
@@ -6,7 +6,7 @@ data = []
 
 def main():
     
-    print("BIENVENIDO AL ROMPABEZAS 4000v7\n\nProfesiones disponibles\n1. Alquimia\n2. Joyería\n")
+    print("BIENVENIDO AL ROMPABEZAS 4000v8\n\nProfesiones disponibles\n1. Alquimia\n2. Joyería\n")
 
     intro()
 
@@ -33,9 +33,9 @@ def intro():
 
 def alquimia():
     print("\nBienvenido al módulo de ALQUIMIA\n\nHaz click derecho para pegar tus datos")
-    print(colored("Cuando termines, presiona ENTER, CTRL+Z, ENTER\n\n","yellow"))
+    print(colored("Cuando termines, presiona ENTER dos veces\n\n","yellow"))
     
-    input_data()
+    input_data2()
     read_data()
 
     #elemT2, elemT3, omnium, primal, vial, agitating, tepid, frost, hochen, focus, saxi
@@ -90,7 +90,7 @@ def joyeria():
     print("\nBienvenido al módulo de JOYERÍA\n\nHaz click derecho para pegar tus datos")
     print(colored("Cuando termines, presiona ENTER, CTRL+Z, ENTER\n\n","yellow"))
     
-    input_data()
+    input_data2()
     read_data()
 
     #engasteT2, engasteT3, polvo, diamante, serevita, orden, ambar, cinto, vial, fuego, tapon, paño
@@ -128,18 +128,31 @@ def viales(vial, fuego, tapon, polvo):
 
 def profit_color(var):
     if var >= 0:
-        return "green"
+        return "light_green"
     else:
-        return "red"
+        return "light_red"
     
 def cat_color(var):
     if var:
-        return "green"
+        return "light_green"
     else:
-        return "red"
+        return "light_red"
 
 def input_data():
     system("TYPE CON > temp.txt")
+
+def input_data2():
+    lines = []
+    while True:
+        line = input()
+        if line:
+            lines.append(line)
+        else:
+            break
+    text = '\n'.join(lines)
+
+    with open("temp.txt", 'w') as file:
+        file.write(text)
 
 def read_data():
     with open("temp.txt", 'r') as file:
